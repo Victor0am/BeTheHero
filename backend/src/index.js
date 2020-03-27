bfae15fc6b64aa410a69663cffe8e4b6045c8e22
Victor Aguiar Marques <victoramarques@hotmail.com>
2020-03-27 15:21:54 -0300

    Frontend do app

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
-app.use(cors());
 /**
  * Métodos HTTP:
  * 
