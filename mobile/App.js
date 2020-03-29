579d67c66cfbd391994d4701ad565399998a33c0
Victor Aguiar Marques <victoramarques@hotmail.com>
2020-03-29 16:00:37 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+
+import Routes from './src/routes';
+
+//Não tem semântica
+//não tem criação de classes
+
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
